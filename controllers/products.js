5aefe772cd9713161db3da88cae3c0419b6209eb
EDWARD ADDO BOAKYE <eboakye151@gmail.com>
2025-03-04 15:54:34 +0000

    Second-commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,23 @@
-export const addProduct = (req, res)=>{
-    res.send('Product added!');
-}
+export const addProduct = (req, res) => {
+  //upload the product image
+  //validate the product information
+  //Save product information in the database
+  //return response
+  res.json(req.body);
+};
 
-export const getProducts = (req, res)=>{
-    res.send('All products!');
-}
+export const getProducts = (req, res) => {
+  res.send("All products!");
+};
 
-export const countProducts = (req , res)=>{
-    res.send('All Product count!');
-}
+export const countProducts = (req, res) => {
+  res.send("All Product count!");
+};
 
-export const updateProduct = (req, res) =>{
-    res.send(`Product with id ${req.params.id} updated!`);
-}
+export const updateProduct = (req, res) => {
+  res.send(`Product with id ${req.params.id} updated!`);
+};
 
-export const deleteProduct = (req , res)=>{
-    res.send(`Products with id ${req.params.id} deleted!`);
-}
+export const deleteProduct = (req, res) => {
+  res.send(`Products with id ${req.params.id} deleted!`);
+};
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from "express";
 import productsRouter from "./routes/products.js";
+import mongoose from "mongoose";
+
+
+//Make Database connection
+
+
+await mongoose.connect(process.env.MONGO_URI);
+
+
 
 //create an express app
 const app = express();
 const port = 3000;
 
+//use global middleware
+app.use(express.json());
+
 //use routes
 app.use(productsRouter);
+
+
 
 //listen for incoming products
 app.listen(port, () => {
